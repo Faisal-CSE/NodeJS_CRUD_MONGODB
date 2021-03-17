daf36addf87eb34faa309817449873b06e066676
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2021-03-17 20:25:37 +0600

    3rd commit

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,17 @@
+const axios = require('axios'); //axios allow to request
+
 exports.homeRoutes = (req,res)=>{
-    res.render('index');
+    //Make a get request to /api/users
+    axios.get("http://localhost:3000/api/users")
+    .then(function(response){
+        //console.log(response);
+        //console.log(response.data);
+        res.render('index',{users: response.data});
+    })
+    .catch(err=>{
+        res.send(err);
+    })
+    //res.render('index',{users: "Faisal porag"}); //Pass the users data to index file
 };
 
 exports.addUserRoutes = (req,res)=>{
@@ -7,5 +19,11 @@ exports.addUserRoutes = (req,res)=>{
 };
 
 exports.updateUserRoutes = (req,res)=>{
-    res.render('update_user');
+    axios.get("http://localhost:3000/api/users",{params: {id: req.query.id}})
+    .then(function(userData){
+        res.render('update_user', {user: userData.data});
+    })
+    .catch(err=>{
+        res.send(err);
+    })
 };
